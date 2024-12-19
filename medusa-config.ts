e668e1256c4f9c43a4a30847511108af4151ab8f
JosehGaks <josehwilddog@gmail.com>
2024-12-19 22:34:23 +0300

    modified:   medusa-config.ts

--- medusa-config.ts
+++ medusa-config.ts
@@ -76,6 +76,16 @@ module.exports = defineConfig({
   },
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    databaseDriverOptions: {
+      connection: {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
     http: {
       storeCors: process.env.STORE_CORS,
       adminCors: process.env.ADMIN_CORS,
